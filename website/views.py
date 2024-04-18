ef51a18e222293753d4304ef5bd673e138d0e11e
jenicekhartigu <honza.hartig@seznam.cz>
2024-04-18 16:50:01 +0200

    day1

--- website/views.py
+++ website/views.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, render_template
-from datetime import datetime
 
 views = Blueprint('views',__name__)
 
 @views.route("/")
 def home():
-    return render_template("index.html", now = datetime.now())
+    return render_template("home.html")
