ad30368116efec8d909178d7716604cca83d8bac
jenicekhartigu <honza.hartig@seznam.cz>
2024-04-22 14:11:48 +0000

    from python anywhere

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from website import create_app
 
-app = create_app()
+# A very simple Flask Hello World app for you to get started with...
 
-if __name__ == '__main__':
-    app.run(debug = True)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
