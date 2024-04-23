72bd10b5bc934713e04d1125df215601d3c74b14
jenicekhartigu <honza.hartig@seznam.cz>
2024-04-23 19:42:06 +0200

    day4

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
 from .person import Person
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 __all__ = ("Person",)
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dasdasdadasdasafa'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
@@ -13,5 +21,14 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
+    import models
+    
+    
+    
     
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
