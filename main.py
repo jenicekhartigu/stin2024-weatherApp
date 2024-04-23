11aae48f3f6ef49a5aac47c8eaeb67be923fff34
jenicekhartigu <honza.hartig@seznam.cz>
2024-04-23 18:32:48 +0200

    main to deploy

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from website import create_app
 
-app = create_app()
+# app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    # app.run(debug = True)
     
-    # test comentar 2
+    create_app = create_app()
+    create_app.run()
+    
+else:
+    gunicorn_app = create_app()
+
+    
     
