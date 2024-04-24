1d92e969eb5624e62fdf41661aa05c0f721784cd
jenicekhartigu <honza.hartig@seznam.cz>
2024-04-24 16:12:16 +0200

    test

--- tests/test_getapidata.py
+++ tests/test_getapidata.py
@@ -10,18 +10,16 @@ from website.getAPIdata import get_api_key, get_weather, get_forecast, getApiDat
 def test_get_weather():
     city = "Prague"
     weather_data = get_weather(city)
-    assert 'error' not in weather_data
+    assert weather_data
 
 
 def test_get_api_key_with_valid_env_variable():
-    """Tests if the function retrieves the API key from a valid environment variable."""
     os.getenv = MagicMock().return_value('valid_api_key')
     api_key = get_api_key()
     assert(api_key)
 
 
 def test_get_api_key_with_missing_env_variable():
-    """Tests if the function returns None when the environment variable is missing."""
     load_dotenv = MagicMock().return_value(None)
     api_key = get_api_key()
     assert(api_key)
