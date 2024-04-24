989af403aba3477c4361d255d2ebe8e8062ce46d
jenicekhartigu <honza.hartig@seznam.cz>
2024-04-24 18:21:12 +0200

    tests calsses

--- tests/test_getapidata.py
+++ tests/test_getapidata.py
@@ -1,18 +1,23 @@
 
-from website.getAPIdata import getApiKey, getWeather, getForecast, getApiData
+from website.getAPIdata import get_weather, get_forecast, get_history, show_weather, current_location
 
 def test_get_weather():
     city = "Prague"
-    weather_data = getWeather(city)
+    weather_data = get_weather(city)
     assert 'error' not in weather_data
 
 def test_get_forecast():
     city = "Prague"
-    weather_data = getForecast(city)
+    weather_data = get_forecast(city)
     assert 'error' not in weather_data
 
-# def test_get_history():
-#     city = "Prague"
-#     days_num = 7
-#     weather_history = get_history(city, days_num)
-#     assert isinstance(weather_history, list)
+def test_get_history():
+    city = "Prague"
+    days_num = 7
+    weather_history = get_history(city, days_num)
+    assert isinstance(weather_history, list)
+
+def test_show_weather():
+    city = "Prague"
+    weather_data, weather_forecast, weather_history, error, loc = show_weather(city)
+    assert error is None
--- website/getAPIdata.py
+++ website/getAPIdata.py
@@ -1,37 +1,67 @@
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from flask import request
 import os
 import requests
 
-def getApiKey():
+def get_api_key():
     load_dotenv()
     api_key = os.getenv('API_KEY')
     return api_key
 
-def getWeather(city):
-    url = f"http://api.weatherapi.com/v1/current.json?key={getApiKey()}&q={city}"
+
+def get_weather(city):
+    url = f"http://api.weatherapi.com/v1/current.json?key={get_api_key()}&q={city}"
     response = requests.get(url)
     data = response.json()
     return data
 
-def getForecast(city):
-    url = f"http://api.weatherapi.com/v1/forecast.json?key={getApiKey()}&q={city}&days=7"
+def get_forecast(city):
+    url = f"http://api.weatherapi.com/v1/forecast.json?key={get_api_key()}&q={city}&days=7"
     response = requests.get(url)
     data = response.json()
     return data
 
-def getApiData(cityName):
-    weather_data = getWeather(cityName)
-    weather_forecast = getForecast(cityName)
+def get_history(city, days_num):
+    end_dt = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+    dt = ((datetime.now() - timedelta(days=1)) - timedelta(days=days_num)).strftime('%Y-%m-%d')
+    url = f"http://api.weatherapi.com/v1/history.json?key={get_api_key()}&q={city}&dt={dt}&end_dt={end_dt}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    filtered_data = []
+    if 'forecast' in data and 'forecastday' in data['forecast']:
+        for forecast_day in data['forecast']['forecastday']:
+            for hour_forecast in forecast_day['hour']:
+                if hour_forecast['time'].endswith('12:00'):
+                    filtered_data.append(hour_forecast)
+    return filtered_data
+
+def show_weather(city):
+    weather_data = get_weather(city)
+    weather_forecast = get_forecast(city)
+    weather_history = get_history(city, 7)
     if 'error' in weather_data or 'error' in weather_forecast:
         error = weather_data['error']['message']
     else:
         error = None
-    
-    loc = weather_data['location']['name']
-    actualTemp = weather_data['current']['temp_c']
-    textWeather = weather_data['current']['condition']['text']
-    imgWeather = weather_data['current']['condition']['icon']
-    
-    # print(type(imgWeather))
-    
-    return loc, actualTemp, textWeather
+    return weather_data, weather_forecast, weather_history, error, city
+
+def current_location():
+    try:
+        ip_address = request.remote_addr
+        if ip_address == '127.0.0.1':
+            city_name = "Liberec"
+            return show_weather(city_name)
+        else:
+            response = requests.get(f"https://ipinfo.io/%7Bip_address%7D/json")
+            if response.status_code == 200:
+                data = response.json()
+                city_name = data.get("city")
+                return show_weather(city_name)
+            else:
+                return None
+    except Exception as e:
+        print("Error:", e)
+        return None
