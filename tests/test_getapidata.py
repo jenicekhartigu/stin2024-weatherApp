98d27eb640338e32adc89492902a94189a6ece8b
jenicekhartigu <honza.hartig@seznam.cz>
2024-04-24 18:12:10 +0200

    tests calsses

--- tests/test_getapidata.py
+++ tests/test_getapidata.py
@@ -1,33 +1,18 @@
-import os
-from dotenv import load_dotenv
-
-import unittest
-from unittest.mock import patch, MagicMock
-
 
 from website.getAPIdata import getApiKey, getWeather, getForecast, getApiData
 
-class TestGetApiKey(unittest.TestCase):
-    def test_getApiKey_return(self):
-        self.assertEqual(getApiKey(), os.getenv('API_KEY'))
+def test_get_weather():
+    city = "Prague"
+    weather_data = getWeather(city)
+    assert 'error' not in weather_data
 
-class TestGetWeather(unittest.TestCase):
-    def test_get_weather_valid_city(self):
-        city = 'London'
-        loc, actualTemp, textWeather = getApiData(city)
-        self.assertEqual(loc,'London')
-        self.assertTrue(isinstance(actualTemp,float))
-        self.assertTrue(isinstance(textWeather, str))
+def test_get_forecast():
+    city = "Prague"
+    weather_data = getForecast(city)
+    assert 'error' not in weather_data
 
-class TestGetForecast(unittest.TestCase):
-    def test_get_forecast_valid_city(self):
-        city = 'London'
-        forecast_data = getForecast(city)
-        self.assertTrue('error' not in forecast_data)
-        self.assertTrue('forecast' in forecast_data)
-        self.assertEqual(forecast_data['location']['name'],city)
-        self.assertEqual(len(forecast_data['forecast']['forecastday']), 7)
-        
-if __name__ == '__main__':
-    unittest.main() 
-
+# def test_get_history():
+#     city = "Prague"
+#     days_num = 7
+#     weather_history = get_history(city, days_num)
+#     assert isinstance(weather_history, list)
