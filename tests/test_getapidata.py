347c889e4821caed120e555bf05c6042a621449c
jenicekhartigu <honza.hartig@seznam.cz>
2024-04-24 17:51:49 +0200

    tests

--- tests/test_getapidata.py
+++ tests/test_getapidata.py
@@ -12,5 +12,18 @@ def test_getApiKey_return():
     assert getApiKey() == os.getenv('API_KEY')
 
     
+def test_get_weather_valid_city():
+    city = 'London'
+    loc, actualTemp, textWeather = getApiData(city)
+    assert loc == 'London'
+    assert isinstance(actualTemp, float)
+    assert isinstance(textWeather, str)
 
 
+def test_get_forecast_valid_city():
+    city = 'London'
+    forecast_data = getForecast(city)
+    assert 'error' not in forecast_data
+    assert 'forecast' in forecast_data
+    assert forecast_data['location']['name'] == city
+    assert len(forecast_data['forecast']['forecastday']) == 7
