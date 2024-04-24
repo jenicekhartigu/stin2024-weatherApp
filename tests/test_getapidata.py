86d6700ff31055703d754e9a6a1e7260f0883824
jenicekhartigu <honza.hartig@seznam.cz>
2024-04-24 18:26:53 +0200

    tests calsses

--- tests/test_getapidata.py
+++ tests/test_getapidata.py
@@ -1,5 +1,5 @@
 
-from website.getAPIdata import get_weather, get_forecast, get_history, show_weather, current_location
+from website.getAPIdata import get_weather, get_forecast, get_history, show_weather, current_location, get_api_key
 
 def test_get_weather():
     city = "Prague"
@@ -7,17 +7,20 @@ def test_get_weather():
     assert 'error' not in weather_data
 
 def test_get_forecast():
+    get_api_key()
     city = "Prague"
     weather_data = get_forecast(city)
     assert 'error' not in weather_data
 
 def test_get_history():
+    get_api_key()
     city = "Prague"
     days_num = 7
     weather_history = get_history(city, days_num)
     assert isinstance(weather_history, list)
 
 def test_show_weather():
+    get_api_key()
     city = "Prague"
     weather_data, weather_forecast, weather_history, error, loc = show_weather(city)
     assert error is None
