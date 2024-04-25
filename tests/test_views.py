cb1a52ac4867f546efebf5e8db3f97ae1afec4eb
jenicekhartigu <honza.hartig@seznam.cz>
2024-04-25 18:51:41 +0200

    CI Update

---
+++ tests/test_views.py
@@ -0,0 +1,18 @@
+from website.tools.views import home, noUserApp, appNoUser, delete_note
+
+def test_delete_note():
+
+    assert True
+    
+    
+def test_appNoUser():
+    
+    assert True
+
+def test_noUserApp():
+    
+    assert True
+    
+def test_home():
+    
+    assert True
