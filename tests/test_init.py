c6ede31653c254bd02f765fa90d6e592d3106a5e
jenicekhartigu <honza.hartig@seznam.cz>
2024-04-25 18:22:02 +0200

    CI Update

---
+++ tests/test_init.py
@@ -0,0 +1,16 @@
+
+import pytest
+from unittest.mock import patch
+from flask import Flask
+from website import create_app
+
+def test_create_app():
+    # Call the function and get the result
+    app = create_app()
+
+    # Check if the result is a Flask application instance
+    assert isinstance(app, Flask)
+
+    # Check if the blueprints are registered correctly
+    assert 'views' in app.blueprints
+    assert 'auth' in app.blueprints
